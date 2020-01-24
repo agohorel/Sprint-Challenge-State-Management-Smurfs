6699faf91b03add30d6e7d92b4255bc5c539d379
agohorel <gohorelalex@gmail.com>
2020-01-24 13:22:50 -0500

    clean up Smurf component functions

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -5,20 +5,12 @@ import { deleteSmurf } from "../actions/deleteSmurf";
 import { editSmurf } from "../actions/editSmurf";
 
 const Smurf = ({ smurf, deleteSmurf, editSmurf, smurfFormData }) => {
-  const removeSmurf = id => {
-    deleteSmurf(id);
-  };
-
-  const updateSmurf = (id, data) => {
-    editSmurf(id, data);
-  };
-
   return (
     <>
       <h2>{smurf.name}</h2>
       <h3>{smurf.age} years old</h3>
       <h3>{smurf.height} tall</h3>
-      <button onClick={() => removeSmurf(smurf.id)}>delete</button>
+      <button onClick={() => deleteSmurf(smurf.id)}>delete</button>
       <button onClick={() => editSmurf(smurf.id, smurfFormData)}>edit</button>
     </>
   );
